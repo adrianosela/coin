1a242dcc672c01e019b5b38378f6692f1b42e56a
Adriano <adriano.selaviles@gmail.com>
2019-10-19 11:41:43 -0700

    add found

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 	"time"
 )
 
-const prev = "2e3a8e88a060cedcd9ac7b74fadd58e0"
-const id = "110b3259f9f189e603ad4142660d6945"
+const (
+	prev  = "2e3a8e88a060cedcd9ac7b74fadd58e0"
+	id    = "110b3259f9f189e603ad4142660d6945"
+	found = "T2JEe2cYC7HC5DMrgmmKiiUnG9XDAuEyW9YftTNyWWU="
+)
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func main() {
+	// blob, _ := base64.StdEncoding.DecodeString(try)
+	// fmt.Println(tryBlob(prev, string(blob), id))
+
 	c, err := Mine(prev, id)
 	if err != nil {
 		log.Fatal(err)
